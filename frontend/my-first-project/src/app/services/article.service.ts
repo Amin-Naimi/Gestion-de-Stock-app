b42148df927ec0990d701b0da5a7ec201e999b9d
Mohamed <ahnbi32@gmail.com>
2023-05-13 17:14:29 +0200

    adding article services

---
+++ frontend/my-first-project/src/app/services/article.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from './user.service';
+import { CategoryService } from './category.service';
+import { HttpClient } from '@angular/common/http';
+import { Article } from '../Models/Article';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+
+  APP_URL = 'http://localhost:8089/gestiondestock/articles';
+
+  constructor(private router : Router,
+    private userService: UserService,
+    private categoryService : CategoryService,
+    private httpClient: HttpClient) {}
+
+    enregisterArticle(article: Article):Observable<Article> {
+      return this.httpClient.post<Article>(`${this.APP_URL}/create`,article);
+    }
+
+    getAllArticles():Observable<Article[]> {
+      return this.httpClient.get<Article[]>(`${this.APP_URL}/all`);
+    }
+
+    findArticleById(idArticle: number):Observable<Article> {
+      return this.httpClient.get<Article>(`${this.APP_URL}/${idArticle}`);
+    }
+/*
+    enregisterArticle(article: Article):Observable<Article> {
+      return this.httpClient.post<Article>(`${this.APP_URL}/`,article);
+    }*/
+}
+
+
